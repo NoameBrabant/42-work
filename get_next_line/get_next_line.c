7f572edfe96ac51c71dd21e9c08818eab4e436c5
NoameBrabant <brabantnoame@gmail.com>
2023-03-21 09:00:16 +0100

    get next line update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -25,14 +25,14 @@ char *get_next_line(int fd)
     int             j;
     
     t = BUFFER_SIZE;
-    res = malloc(9);
+    res = malloc(1);
    /* i = 0;
     while (i < 8)
     {
         res[i] ='a' +i;
         i++;
-    }
-    res[i] = '\0';*/
+    }*/
+    res[0] = '\0';
     printf("On a alloue res\n");
     printf(C_RED"%s\n"C_NOR, res);
     printf(C_BLU"%s\n\n"C_NOR, stash);
@@ -58,20 +58,22 @@ char *get_next_line(int fd)
         printf(C_RED"%s\n"C_NOR, res);
         printf(C_BLU"%s\n"C_NOR, stash);
         printf(C_GRE"%s\n\n"C_NOR, tmp);
-        tmp = NULL;
+        tmp[0] = '\0';
         n = ft_strchr(res, '\n');
         while (n == -1 && t == BUFFER_SIZE)
         {
             //printf("oui1");
             t = read(fd, buff, BUFFER_SIZE);
             buff[t] = '\0';
+            printf(C_YEL"%d\n"C_NOR, buff);
             tmp = ft_strjoin(res, buff);
+            printf(C_YEL"%s\n"C_NOR, tmp);
             free(res);
             res = tmp;
             printf("On est en train de remplir res\n");
             printf(C_RED"%s\n"C_NOR, res);
             printf(C_BLU"%s\n\n"C_NOR, stash);
-          //  free(tmp);
+            //free(tmp);
             n = ft_strchr(res, '\n');
         }
         printf("On est avant de remplir stash\n");
@@ -80,18 +82,19 @@ char *get_next_line(int fd)
         i = 0;
         n = ft_strchr(res, '\n');
         //printf("\nn ===== %d || res ===== %c\n", n, res[n - 1]);
-        while (res[n + i + 1])
+        while (res[n + i + 1] != '\0')
         {
             //printf("oui2");
             stash[i] = res[n + i + 1];
             i++;
         }
+        stash[i] = '\0';
        // stash = ft_stash(res, n);
         //printf("\nn ===== %d || res[n] ===== %c\n", n, res[n - 1]);
         printf("On a fini de remplir stash\n");
         printf(C_RED"%s\n"C_NOR, res);
         printf(C_BLU"%s\n\n"C_NOR, stash);
-        i = ft_strlen(res) - 1;
+        i = ft_strlen(res);
         while (i > n)
         {
             //printf("oui3");
